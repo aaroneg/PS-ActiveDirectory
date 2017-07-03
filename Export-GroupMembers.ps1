1103be533c74860a6ada601489d68f1d9ade2719
Aaron Glenn <aglenn@laureateinstitute.org>
2017-07-03 14:59:33 -0500

    Export-GroupMembers.ps1

---
+++ Export-GroupMembers.ps1
@@ -0,0 +1,5 @@
+﻿# The name of the group whose members we need
+$group="Group1"
+# Read all members of the group
+$members=Get-ADGroupMember $group
+$members | Select-Object @{Name="Name";Expression={$_.SamAccountName}} | Export-Csv -NoTypeInformation -Append -Path "$PSScriptRoot\GroupMembers.csv"
