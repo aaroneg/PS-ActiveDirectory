f5321c690e814632cd7450fc50eb0fadefd21523
Aaron <aaroneg@users.noreply.github.com>
2017-07-20 13:11:59 -0500

    Update Export-GroupMembers.ps1

--- Export-GroupMembers.ps1
+++ Export-GroupMembers.ps1
@@ -2,4 +2,4 @@
 $group="Group1"
 # Read all members of the group
 $members=Get-ADGroupMember $group
-$members | Select-Object @{Name="Name";Expression={$_.SamAccountName}} | Export-Csv -NoTypeInformation -Append -Path "$PSScriptRoot\GroupMembers.csv"
+$members | Select-Object @{Name="Name";Expression={$_.SamAccountName}} | Export-Csv -NoTypeInformation -Append -Path "$PSScriptRoot\$($Group)Members.csv"
