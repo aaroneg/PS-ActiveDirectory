d907def059b548cb238e1791b2d8753b16e9b5d1
Aaron Glenn <aglenn@laureateinstitute.org>
2018-04-23 11:35:43 -0500

    Add progress indicators

--- Get-ADHealth.ps1
+++ Get-ADHealth.ps1
@@ -19,6 +19,7 @@ if (!$PSScriptRoot){$PSScriptRoot=$PWD}
 $Report=@()
 Clear-Host
 $ADForest = [system.directoryservices.activedirectory.Forest]::GetCurrentForest()
+Write-Host "Reading forest config"
 $Report+="# Forest"
 $Report+="* Forest Name:                $($ADForest.Name)"
 $Report+="`n## Forest Role Owners"
@@ -29,12 +30,12 @@ $NamingRoleOwnerContactable=Test-Connection($ADForest.NamingRoleOwner) -Count 1 
 $Report+="`n## Role Owner Online"
 $Report+="* Schema Role Owner Contactable: **$SchemaRoleOwnerContactable**"
 $Report+="* Naming Role Owner Contactable: **$NamingRoleOwnerContactable**"
-
+Write-Host "Getting Domains in Forest"
 $Report+="`n## Forest Domains"
 foreach ($ForestDomain in $ADForest.Domains) {
     $Report+="* $($ForestDomain.Name)"
 }
-
+Write-Host "Getting Domain info for Current Domain"
 $ADDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
 $Report+="`n# Current Domain"
 $Report+="* Domain Name:                    $($ADDomain.Name)"
@@ -51,47 +52,68 @@ $Report+="* RID contactable : **$RIDContactable**"
 $Report+="* INF contactable : **$InfraContactable**"
 $Report+="`n## Domain Controllers"
 $ADControllers=(Get-ADDomain).ReplicaDirectoryServers
+Write-Host "Checking domain controllers & service status"
+
+$i=0
 Foreach ($ADController in $ADControllers) {
+    Write-Progress -Activity "Pinging AD controllers" -Status $ADController -PercentComplete ($i/$ADControllers.Count)
     if(Test-Connection $ADController -Count 1 -Quiet){$Status="**Online**"}else{$Status="**Offline**"}
     $Report+="* "+$ADController+" : "+$Status
+    $i++
 }
 
+$i=0
 $Report+="`n## NetLogon Services"
 Foreach ($ADController in $ADControllers) {
+    Write-Progress -Activity "Checking NetLogon Service" -Status $ADController -PercentComplete ($i/$ADControllers.Count)
     $Status=(Get-Service -ComputerName $ADController -Name Netlogon -ErrorAction SilentlyContinue).Status 
     $Report+="* "+$ADController+" : **"+$Status+"**"
+    $i++
 }
 
+$i=0
 $Report+="`n## NTDS Services"
 Foreach ($ADController in $ADControllers) {
+    Write-Progress -Activity "Checking NTDS Service" -Status $ADController -PercentComplete ($i/$ADControllers.Count)
     $Status=(Get-Service -ComputerName $ADController -Name NTDS -ErrorAction SilentlyContinue).Status 
     $Report+="* "+$ADController+" : **"+$Status+"**"
+    $i++
 }
 
+$i=0
 $Report+="`n## DNS Services"
 Foreach ($ADController in $ADControllers) {
+    Write-Progress -Activity "Checking DNS Service" -Status $ADController -PercentComplete ($i/$ADControllers.Count)
     $Status=(Get-Service -ComputerName $ADController -Name DNS -ErrorAction SilentlyContinue).Status 
     $Report+="* "+$ADController+" : **"+$Status+"**"
+    $i++
 }
 
-
+$i=0
 $Report+="`n## KDC Services"
 Foreach ($ADController in $ADControllers) {
+    Write-Progress -Activity "Checking KDC Service" -Status $ADController -PercentComplete ($i/$ADControllers.Count)
     $Status=(Get-Service -ComputerName $ADController -Name KDC -ErrorAction SilentlyContinue).Status 
     $Report+="* "+$ADController+" : **"+$Status+"**"
+    $i++
 }
 
-
+$i=0
 $Report+="`n## ADWS Services"
 Foreach ($ADController in $ADControllers) {
+    Write-Progress -Activity "Checking ADWS Service" -Status $ADController -PercentComplete ($i/$ADControllers.Count)
     $Status=(Get-Service -ComputerName $ADController -Name ADWS -ErrorAction SilentlyContinue).Status 
     $Report+="* "+$ADController+" : **"+$Status+"**"
+    $i++
 }
 
+$i=0
 $Report+="`n## Secure Channel to Domain Controllers"
 Foreach ($ADController in $ADControllers) {
+    Write-Progress -Activity "Testing Secure Channel to Domain Controllers" -Status $ADController -PercentComplete ($i/$ADControllers.Count)
     $Status=Test-ComputerSecureChannel -Server $ADController
     $Report+="* "+$ADController+" : **"+$Status+"**"
+    $i++
 }
 
 $ReplFailures = (Get-ADReplicationFailure -Scope Domain -ErrorAction SilentlyContinue | where FailureCount -gt 0)
