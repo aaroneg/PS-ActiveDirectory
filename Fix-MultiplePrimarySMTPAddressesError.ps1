a4bad554ed562331a3f9271d141ab8e67f26e884
Aaron <aaroneg@users.noreply.github.com>
2018-04-25 13:43:37 -0500

    Update Fix-MultiplePrimarySMTPAddressesError.ps1

--- Fix-MultiplePrimarySMTPAddressesError.ps1
+++ Fix-MultiplePrimarySMTPAddressesError.ps1
@@ -1,7 +1,17 @@
+<#
+    .SYNOPSIS
+        Removes "username@libr.net" SMTP address, which makes the account valid for use with exchange
+    .EXAMPLE
+        Fix-MultiplePrimarySMTPAddresses.ps1 -Username jdoe -domain "contoso.com"
+#>
+
 [CmdletBinding()]
 PARAM(
   [parameter(Mandatory=$true)][string]$Username
   [parameter($Mandatory=$true)[string]$DomainNameToRemove
 )
+<#
+This fixes an issue where exchange complains that an object is corrupt because there are multiple primary smtp addresses
+#>
 $User=Get-ADUser $Username
 Set-ADUser $User -Remove @{ProxyAddresses="SMTP:$($Username)@$($DomainNameToRemove)"} -Verbose
